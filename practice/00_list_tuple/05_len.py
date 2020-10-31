0c850282deda8b793b0f2811a2ba66d59e738df8
jiseongg <jiseongg301@gmail.com>
2020-10-31 16:28:36 +0900

    [practice/00_list_tuple]: add practice for list/tuple (2)

---
+++ practice/00_list_tuple/05_len.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+lst = [1, 2, 3]
+tup = (1, 2, 3)
+dct = {'a': 1, 'b': 2, 'c': 3}
+np_arr = np.array([1, 2, 3])
+
+print("len(lst): %d" % len(lst))
+print("len(tup): %d" % len(tup))
+print("len(dct): %d" % len(dct))
+print("len(np_arr): %d" % len(np_arr))
